c628161d3a6820b9aeee3c69e8e22e095903f596
Bakrifly <bakri.hmouda@gmail.com>
2022-01-03 17:59:18 +0200

    Step 3 - Create an express simple API

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ app.listen(3003, function(){console.log("app is listening  to port 3003")})
 
 
 app.get("/", (req, res)=>{res.sendStatus(200);})
+
+app.get("/test", (req, res)=>{res.json({status: 200, Message:"ok"})})
+
+function getTime(){
+    const today = new Date();
+    return (`${today.getHours()}:${today.getMinutes()}`)
+}
+
+app.get("/time", (req, res)=>{res.json({status:200, Message: getTime()})})
+
+
+
