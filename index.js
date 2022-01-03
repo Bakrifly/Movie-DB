a4bca4ee7a025dad9bec577a6234e78d3cd16268
Bakrifly <bakri.hmouda@gmail.com>
2022-01-03 19:46:59 +0200

    Step 4 - Let's complicate the API

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 
 app.listen(3003, function(){console.log("app is listening  to port 3003")})
 
-
+//==== step 2  ====
 app.get("/", (req, res)=>{res.sendStatus(200);})
 
+
+//==== step 3 test route ====
 app.get("/test", (req, res)=>{res.json({status: 200, Message:"ok"})})
 
+//---- step 3 time route ----
 function getTime(){
     const today = new Date();
     return (`${today.getHours()}:${today.getMinutes()}`)
@@ -16,4 +19,15 @@ function getTime(){
 app.get("/time", (req, res)=>{res.json({status:200, Message: getTime()})})
 
 
+//==== step 4 Hello route ====
+app.get("/hello", (req, res)=>{res.json({status:200, message:"Hello!"})})
+app.get("/hello/:ID", (req, res)=>{res.json({status:200, message:`Hello, ${req.params.ID}!`})})
 
+//---- step 4 search api ----
+app.get("/Search", (req, res)=>{
+    const searchQuery = req.query.s;
+    
+    if (searchQuery === "" || searchQuery === undefined){res.status(500).json({status: 500, error:true, message: "you have to provide a search query"})
+
+    } else {res.status(200).json({status: 200, message: "ok", data: searchQuery})}
+})
