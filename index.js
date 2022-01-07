ea09626d02d609633c9d8a0714a712a26c843d08
Bakrifly <bakri.hmouda@gmail.com>
2022-01-08 00:47:04 +0200

    Step 12: Data Persistence

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+// const bodyParser= require('body-parser');
 
-app.listen(3003, function(){console.log("app is listening  to port 3003")})
+//==== step 12 MongoDB ====
+
+
+const url = "mongodb+srv://bakri:0000@cluster0.m9dax.mongodb.net/MoviesDB?retryWrites=true&w=majority"
+const connectionParams = {useNewUrlParser: true, useUnifiedtopology: true}
+
+mongoose.connect(url, connectionParams).then(()=>{console.log("connected to Database successfully")}).catch((err)=>{console.log(`something went wrong ${err}`)})
+
+const Schema = mongoose.Schema;
+
+const moviesDbSchema = new Schema({
+    title:{type: String, required: true},
+    year:{type: Number, required: true},
+    rating:{type: Number, required: true, default: 4}
+});
+
+const movies = mongoose.model("movies", moviesDbSchema)
+
+const DB = ()=> { return ( movies.find());}
+
+
 
 //==== step 2  ====
 app.get("/", (req, res)=>{res.sendStatus(200);})
@@ -33,18 +55,10 @@ app.get("/Search", (req, res)=>{
 })
 
 
-//==== step 5 CRUD ====
-
-const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-]
 
 
 app.get("/movies/create", (req, res)=>{});
-app.get("/movies/read", (req, res)=>{res.json({status: 200, data: movies})});
+app.get('/movies/read', async (req, res) => {res.send({data: await movies.find()})});
 app.get("/movies/update", (req, res)=>{});
 app.get("/movies/delete", (req, res)=>{});
 
@@ -52,62 +66,63 @@ app.get("/movies/delete", (req, res)=>{});
 //==== step 6 search ====
 
 app.get("/movies/read/by-date", (req, res)=>{ 
-    res.json({status:200, data: movies.sort((a,b)=>{return (b.year - a.year)})})   
+    movies.find()
+    .then(result => {res.json({status:200, data: result.sort((a,b)=>{return (b.year - a.year)})})})   
 })
 
 app.get("/movies/read/by-rating", (req, res)=> {
-    res.json({status:200, data: movies.sort((a, b)=>{return (b.rating -a.rating)})})
+    movies.find()
+    .then(result => { res.json({status:200, data: result.sort((a, b)=>{return (b.rating -a.rating)})})})
 })
 
 app.get("/movies/read/by-title", (req, res)=>{
-    res.json({status:200, data: movies.sort(function (a, b) {
+    movies.find()
+    .then(result => {res.json({status:200, data: result.sort(function (a, b) {
         return a.title.localeCompare(b.title);
-      })})
+      })})})
 })
 
 
 //==== step 7 Read one ====
 
-app.get("/movies/read/id/:ID", (req, res)=>{
+app.get("/movies/read/id/:ID",  (req, res)=>{
 
-    let ID = parseInt(req.params.ID)
-    if (ID >= movies.length || ID < 0)
-        {res.status(404).json({status: 404, error:true, message: "the movie <ID> does not exist"})}
-    else {
-        res.json({status:200, Data:movies[ID]})
-    }
+    let ID = req.params.ID
+    movies.findById(ID)
+    .then(result =>{res.status(200).json({status:200, Data: result})})
+    .catch(err=>{res.status(404).json({status:404, error:true, message:"the movie <ID> does not exist"}) })
+
 }
 )
 
 
 //==== step 8 CREATE ====
 
-app.post("/movies/add", (req, res)=>{
+app.post("/movies/add", async(req, res)=>{
 
     const title = req.query.title;
     const year = parseInt(req.query.year);
-    const rating = req.query.rating || 4;
+    const rating = parseInt(req.query.rating) || 4;
 
     if(title === undefined || title ==="" || isNaN(year) || year.toString().length < 4)
         {res.status(403).json({status: 403, error: true, message: "you cannot creat a movie without providing a title and a year"})}
     else {
-        movies.push({title: title, year: year, rating: rating})
-        res.status(200).json({status:200, data: movies})
+       movies.create({ title: title, year: year, rating: rating })
+        .then(res.status(200).json({status:200, data: await DB()}))
+        .catch(err=>{err})
         }
 }
 )
 
 //==== step 9 DELETE ====
 
-app.delete("/movies/delete/:ID", (req, res)=>{
+app.delete("/movies/delete/:ID", async (req, res)=>{
 
     const ID = req.params.ID
 
-    if(ID < 0 || ID >= movies.length)
-        {res.status(404).json({status:404, error: true, message:"the movie <ID> does not exist"})}
-    else
-        {movies.splice(ID, 1);
-        res.status(200).json({status:200, Data: movies})}
+    movies.findByIdAndDelete(ID)
+    .then( res.status(200).json({status:200,message:"Deleted successfully", data: await DB()}) )
+    .catch( err =>{res.status(400).json({status: 400, error:true, message: err})} )
 })
 
 
@@ -115,21 +130,26 @@ app.delete("/movies/delete/:ID", (req, res)=>{
 
 app.patch("/movies/update/:ID", (req, res)=>{
 
-    const ID = parseInt(req.params.ID);
+    const ID = req.params.ID;
     const title = req.query.title;
     const year = parseInt(req.query.year);
     const rating = parseInt(req.query.rating);
 
-    if(ID >= movies.length || ID < 0)
-    {res.status(404).json({status:404, error: true, message:"the movie <ID> does not exist"})}
 
-    else {
+movies.findById(ID)
+    .then((result)=>{
         if(title !== undefined && title !== "")
-            {movies[ID].title = title};
+            {result.title = title};
         if(!isNaN(year) && year.toString().length === 4 )
-            {movies[ID].year = year};
+            {result.year = year};
         if(rating !== undefined && rating !== "" && !isNaN(rating) )
-            {movies[ID].rating = rating};
-    
-    res.status(200).json({status:200, data: movies})
-}})
+            {result.rating = rating};
+    result.save()
+    movies.find().then( result=>{res.status(200).json({status:200, message:"updated successfully", data: result})})
+    })
+    .catch(err=>{res.status(404).json({status:404, error: true, message: err})})
+})
+
+
+
+app.listen(3005, function(){console.log("app is listening  to port 3005")})
